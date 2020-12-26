2a175654688b4817b4d757bbff0a42002e0b46e7
Bailey Moir <bmo4801@stacmail.net>
2020-12-26 18:32:09 +1300

    stuff from phone

---
+++ src/engine/Component.java
@@ -0,0 +1,13 @@
+package engine.components;
+
+import engine.GameObject;
+
+class Component {
+   abstract private GameObject obj;
+   
+   abstract public Class classType'
+   
+   Component(GameObject object) {
+      obj = Object
+   }
+}
